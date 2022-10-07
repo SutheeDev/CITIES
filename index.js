b88bf076e03b8d5c04d253069ab0f2cab49df525
SU-T <guitaristpeet@gmail.com>
2022-10-07 16:56:32 -0700

    add h1 fade-in

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const allWords = document.querySelectorAll('h1');
+const allWordsTl = gsap.timeline();
+allWordsTl
+    .set(allWords, {opacity: 0})
+    .to(allWords, {opacity: 1})
+
 const words = document.querySelectorAll('span');
 words.forEach((word) => {
     // Get the width of each word
