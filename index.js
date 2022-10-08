29abb3183b6d7e15e0d7819325c10360a8a95d72
SU-T <guitaristpeet@gmail.com>
2022-10-07 17:08:59 -0700

    refactor fade-in to get stagger effect

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const allWords = document.querySelectorAll('h1');
-const allWordsTl = gsap.timeline();
-allWordsTl
-    .set(allWords, {opacity: 0})
-    .to(allWords, {opacity: 1})
+const lines = document.querySelectorAll('h1');
+const linesTl = gsap.timeline();
+linesTl
+    .set(lines, {opacity: 0})
+    .to(lines, {opacity: 1, stagger: 0.1})
+lines.forEach((line) => {
+
+})
 
 const words = document.querySelectorAll('span');
 words.forEach((word) => {
     // Get the width of each word
     const wordWidth = word.offsetWidth;
+
+    for (i = 0; i < 20; i++){
+        word.cloneNode(true)
+    }
+
 });
 
