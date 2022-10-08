26efa89d13c934d8d55eb1d6b8bb949fbaaf284f
SU-T <guitaristpeet@gmail.com>
2022-10-07 17:36:40 -0700

    clone words

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
+// Fade all the line in with stagger effect
 const lines = document.querySelectorAll('h1');
 const linesTl = gsap.timeline();
 linesTl
     .set(lines, {opacity: 0})
     .to(lines, {opacity: 1, stagger: 0.1})
+
 lines.forEach((line) => {
-
-})
-
-const words = document.querySelectorAll('span');
-words.forEach((word) => {
+    const word = line.querySelector('span');
+    
     // Get the width of each word
     const wordWidth = word.offsetWidth;
-
-    for (i = 0; i < 20; i++){
-        word.cloneNode(true)
+    
+    for (i = 0; i < 10; i++){
+        line.appendChild(word.cloneNode(true));
     }
-
-});
-
+})
